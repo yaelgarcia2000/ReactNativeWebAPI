1e059b2a614a61778be6c42961c170825e8a33e5
yaelgarcia2000 <yaelgc2000@gmail.com>
2022-06-22 03:07:58 -0500

    Alert

--- proyectapp/App.js
+++ proyectapp/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  Image,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+  Alert,
+} from 'react-native';
 const App = () => {
+  const IngresarDatos = () => {
+    Alert.alert('Aviso', 'Dio clic en ingresar');
+  };
   return (
-    <>
-      <Text style={styles.titulo}>Página Login</Text>
-    </>
+    <View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.titulo}>Página Login</Text>
+        <View style={styles.avatar}>
+          <Image
+            style={styles.imagen}
+            source={require('./src/assest/images/Caricatura.jpg')}
+          />
+        </View>
+        <View style={styles.formulario}>
+          <Text style={styles.styleLabelForm}>Usuario:</Text>
+          <TextInput style={styles.styleInput} />
+          <Text style={styles.styleLabelForm}>Contraseña:</Text>
+          <TextInput style={styles.styleInput} secureTextEntry={true} />
+          <View style={styles.styleButton}>
+            <Button title="Ingresar" onPress={IngresarDatos} />
+            <Button title="Registra" />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 const styles = StyleSheet.create({
@@ -15,6 +45,40 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     color: 'white',
     paddingVertical: 20,
+  },
+  avatar: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'red',
+  },
+  imagen: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginVertical: 30,
+    borderRadius: 100,
+  },
+  formulario: {
+    margin: 20,
+    backgroundColor: 'yellow',
+  },
+  styleLabelForm: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: 'black',
+    marginVertical: 10,
+  },
+  styleInput: {
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 0.2,
+    borderRadius: 10,
+    fontSize: 16,
+  },
+  styleButton: {
+    marginTop: 20,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
   },
 });
 export default App;
