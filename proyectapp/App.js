7e14974fbd6118e0203594abeeae6996401db53b
yaelgarcia2000 <yaelgc2000@gmail.com>
2022-06-22 03:53:24 -0500

    asignar valores TextInput

--- proyectapp/App.js
+++ proyectapp/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Text,
   StyleSheet,
@@ -10,8 +10,29 @@ import {
   Alert,
 } from 'react-native';
 const App = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
   const IngresarDatos = () => {
-    Alert.alert('Aviso', 'Dio clic en ingresar');
+    Alert.alert(
+      'Aviso',
+      'Dio clic en Ingresar: \n' +
+        '\nEl usuario es: ' +
+        user +
+        ' \ncontraseña: ' +
+        password,
+    );
+  };
+  const Registrar = () => {
+    Alert.alert('Aviso', 'Dio clic en Registrar', [
+      {
+        text: 'Aceptar',
+        onPress: () => Alert.alert('Aviso', 'Dio clic en Aceptar'),
+      },
+      {
+        text: 'Cancelar',
+        onPress: () => Alert.alert('Aviso', 'Dio clic en Cancelar'),
+      },
+    ]);
   };
   return (
     <View>
@@ -25,12 +46,20 @@ const App = () => {
         </View>
         <View style={styles.formulario}>
           <Text style={styles.styleLabelForm}>Usuario:</Text>
-          <TextInput style={styles.styleInput} />
+          <TextInput
+            style={styles.styleInput}
+            keyboardType="default"
+            onChangeText={value => setUser(value)}
+          />
           <Text style={styles.styleLabelForm}>Contraseña:</Text>
-          <TextInput style={styles.styleInput} secureTextEntry={true} />
+          <TextInput
+            style={styles.styleInput}
+            secureTextEntry={true}
+            onChangeText={value => setPassword(value)}
+          />
           <View style={styles.styleButton}>
             <Button title="Ingresar" onPress={IngresarDatos} />
-            <Button title="Registra" />
+            <Button title="Registra" onPress={Registrar} />
           </View>
         </View>
       </ScrollView>
